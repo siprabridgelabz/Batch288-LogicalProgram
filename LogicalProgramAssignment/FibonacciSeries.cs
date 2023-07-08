d51904bba9e80200dfb877e1b18796bec1ce730a
sipra <sipramishra085@gmail.com>
2023-07-08 14:08:25 +0530

    [Sipra]Add:Find fibonacci Series

---
+++ LogicalProgramAssignment/FibonacciSeries.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalProgramAssignment
+{
+     class FibonacciSeries
+    {
+        public void Series()
+        {
+            int a = 0;
+            int b = 1;
+
+            Console.WriteLine("Enter a value");
+            int n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(b);
+            for (int i = 0; i <= n; i++)
+            {
+                int f = a + b;
+                Console.WriteLine(f);
+                a = b;
+                b = f;
+            }
+        }
+    }
+}
---
+++ LogicalProgramAssignment/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalProgramAssignment
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine("Welcome to Logical Programs");
+
+                Console.WriteLine("1-FibonacciSeries");
+                {
+                    Console.WriteLine("CHOOSE THE BELOW OPTIONS");
+                    int option = Convert.ToInt32(Console.ReadLine());
+                    switch (option)
+                    {
+                        case 1:
+                            Console.WriteLine("Fibonacci Series Program");
+                            FibonacciSeries fibonacci = new FibonacciSeries();
+                            fibonacci.Series();
+                            break;
+                        default:
+                            Console.WriteLine("Invalid Option");
+                            break;
+                    }
+                }
+            }
+        }
+    }
+}
